77275cf9fbf34b9ad0c718b88b39de6fec1c46b6
MUHAMMADAIZAZ-TECH <aizaznadeem174@gmail.com>
2023-04-09 03:51:39 -0700

    cors

--- index.js
+++ index.js
@@ -26,13 +26,7 @@ app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
 //middlewares
-app.use(
-  cors({
-    origin: [process.env.CLIENT_ORIGIN],
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
